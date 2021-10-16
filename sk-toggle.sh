ab0e899136ffe79765404452f4c559391a592d67
CieanYau <yimengxiaoyao@foxmail.com>
2021-10-16 10:38:32 +0800

    Update

---
+++ sk-toggle.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+result=$(ps ax|grep -v grep|grep screenkey)
+if [ "$result" == "" ]; then
+  eval "screenkey --bg-color white --font-color black --font SauceCodePro --opacity 0.9 -t 2 &"
+else
+  eval "killall screenkey"
+fi
